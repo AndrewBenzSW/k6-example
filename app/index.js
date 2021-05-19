6d0c9acaf176b744b28ca2a1f5687b417cbf59fe
Andrew Benz <andrew.benz@auctane.com>
2021-05-19 11:48:44 -0500

    Add simple example app

---
+++ app/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+};
+
+app.use(express.json());
+
+// Simple Hello World
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Add two addends
+app.get("/add/:addend1/:addend2", (req, res) => {
+  res.send(
+    (
+      parseInt(req.params["addend1"], 10) + parseInt(req.params["addend2"], 10)
+    ).toString()
+  );
+});
+
+// Calculate the sum of the addends
+app.post("/sum", (req, res) => {
+  res.json({
+    result: req.body.addends.reduce((sum, addend) => sum + addend, 0),
+  });
+});
+
+app.get("/random", (req, res) => {
+  res.send(getRandomIntInclusive(1, 10).toString());
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
